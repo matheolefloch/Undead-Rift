4fbf57f9799abe7c7c0fa76d8c1c4cb126fb6aab
Romain <103925557+rmgn17@users.noreply.github.com>
2024-02-03 11:42:44 +0100

    run anim

--- Undead Rift/Assets/Scripts/PlayerAnimation.cs
+++ Undead Rift/Assets/Scripts/PlayerAnimation.cs
@@ -14,8 +14,10 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool isRunning = animator.GetBool("isRunning");
         bool isWalking = animator.GetBool("isWalking");
-        bool forwardPressed = Input.GetKey("q");
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
         
         if(!isWalking && forwardPressed)
         {
@@ -24,6 +26,14 @@ public class NewBehaviourScript : MonoBehaviour
         if(isWalking && !forwardPressed)
         {
             animator.SetBool("isWalking", false);
+        }
+        if(!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        if(isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool("isRunning", false);
         }
     }
 }
