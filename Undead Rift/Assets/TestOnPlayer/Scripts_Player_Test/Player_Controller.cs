59a95a9dc59c2a0c80f38b14567ce61779b4a58d
Romain <103925557+rmgn17@users.noreply.github.com>
2024-03-16 18:18:28 +0100

    import pistol anim

--- Undead Rift/Assets/TestOnPlayer/Scripts_Player_Test/Player_Controller.cs
+++ Undead Rift/Assets/TestOnPlayer/Scripts_Player_Test/Player_Controller.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     public bool CanMove {get; private set;} = true;
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+
+    [Header("Functional Options")]
+    [SerializeField] private bool canSprint = true;
+
+    [Header("Functional Options")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
 
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintSpeed = 6.0f;
     [SerializeField] private float gravity = 30.0f;
 
     [Header("Look Parameters")]
@@ -43,7 +51,7 @@ public class Player_Controller : MonoBehaviour
 
     private void HandleMovementInput()
     {
-        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+        currentInput = new Vector2((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
 
         float moveDirectionY = moveDirection.y;
         moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
