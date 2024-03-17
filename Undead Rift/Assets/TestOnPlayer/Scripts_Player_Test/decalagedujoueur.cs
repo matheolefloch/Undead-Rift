dde34247d86b298813c16176f2f0cfa66e501dbb
Romain <103925557+rmgn17@users.noreply.github.com>
2024-03-17 21:34:23 +0100

    re marche et course

--- Undead Rift/Assets/TestOnPlayer/Scripts_Player_Test/decalagedujoueur.cs
+++ Undead Rift/Assets/TestOnPlayer/Scripts_Player_Test/decalagedujoueur.cs
@@ -7,7 +7,7 @@ public class decalagedujoueur : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.tag = "decalage";
+        
     }
 
     // Update is called once per frame
