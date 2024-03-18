f0dee81001d10ba6753ea827c0b783e896f67a94
matheo.le-floch <matheolefloch@gmail.com>
2024-03-18 21:26:49 +0100

    Lobby lancement multijoueur

--- Undead Rift/Assets/Scripts/Launch1.cs
+++ Undead Rift/Assets/Scripts/Launch1.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-
+using TMPro;
+using UnityEngine.SceneManagement;
 public class Launch1 : MonoBehaviour
 {
     private int player;
     [SerializeField] CountPlayer pl;
     private int MaxPlayer;
     private bool TimerOn;
-
+    private float TimeLeft;
+    public TextMeshProUGUI tex;
     void Start()
     {
         TimerOn = false;
         player = 0;
-        MaxPlayer = pl.FindPlayersByTag();
     }
 
     void OnTriggerEnter(Collider other)
@@ -31,20 +32,75 @@ public class Launch1 : MonoBehaviour
             player--;
         }
     }
-
-    // Start is called before the first frame update
-    
-
-    // Update is called once per frame
     void Update()
     {
+        MaxPlayer = pl.FindPlayersByTag();
         if (TimerOn)
-         {
-            Debug.Log("ça marche");
-        }
-        else if (player >= MaxPlayer/2)
+        {
+            if (TimeLeft < 1) 
             {
-                TimerOn = true;
+                SceneManager.LoadScene("MAP1");
             }
+            else if (!Valid1(player, MaxPlayer)) 
+            {
+                TimerOn = false;
+                tex.enabled = false;
+            }
+            else if (Valid2(player, MaxPlayer) && TimeLeft > 30)
+            {
+                TimeLeft = 30;
+            }
+            else if (Valid3(player, MaxPlayer) && TimeLeft > 10)
+            {
+                TimeLeft = 10;
+            }
+            TimeLeft -= Time.deltaTime;
+            Timer(Mathf.FloorToInt(TimeLeft));
+        }
+        else if (Valid1(player, MaxPlayer))
+        {
+            TimerOn = true;
+            TimeLeft = 60;
+            tex.enabled = true;
+        }
+        else if (Valid2(player, MaxPlayer))
+        {
+            TimerOn = true;
+            TimeLeft = 30;
+            tex.enabled = true;
+        }
+                else if (Valid1(player, MaxPlayer))
+        {
+            TimerOn = true;
+            TimeLeft = 10;
+            tex.enabled = true;
+        }
+    }
+
+    private bool Valid1(int player, int MaxPlayer)
+    {
+        if (player == MaxPlayer)
+            return true;
+        return false;
+    }
+
+    private bool Valid2(int player, int MaxPlayer)
+    {
+        if (player >= (MaxPlayer/4)*3 + MaxPlayer%2)
+            return true;
+        return false;
+    }
+    private bool Valid3(int player, int MaxPlayer)
+    {
+        if (player >= MaxPlayer/2 + MaxPlayer%2)
+            return true;
+        return false;
+    }
+
+    void Timer(int tim) 
+    {
+        if (tim == 1 || tim == 0)
+            tex.text = $"Lancement dans {tim} seconde.";
+        else tex.text = $"Lancement dans {tim} secondes.";
     }
 }
